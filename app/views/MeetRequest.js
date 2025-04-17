2196be774ebfc2aabd9c52c79c9086721099d443
Olgay <olgayaslandag@gmail.com>
2025-04-17 22:49:42 +0300

    update

--- app/views/MeetRequest.js
+++ app/views/MeetRequest.js
@@ -3,7 +3,6 @@ import styleAuth from "../styles/styleAuth";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import { useEffect, useState } from "react";
 import LayoutLock from "./LayoutLock";
-import { Picker } from '@react-native-picker/picker';
 import { useDispatch, useSelector } from "react-redux";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { FontAwesome } from '@expo/vector-icons';
@@ -12,10 +11,11 @@ import { MeetStoreApi } from "../api/MeetApi";
 
 
 export default function MeetRequest() {
-    const [form, setForm] = useState({name: '', email: '', phone: '', company_name: '', date: new Date()});
+    const [form, setForm] = useState({name: '', email: '', phone: '', company_name: '', date: new Date(), user_id: 0, person: 1});
     const auth = useSelector(state => state.auth.value);    
     const [showDatePicker, setShowDatePicker] = useState(false);
     const [success, setSuccess] = useState(false);
+    const [errors, setErrors] = useState(false);
     const dispatch = useDispatch();
 
 
@@ -26,18 +26,37 @@ export default function MeetRequest() {
             email: auth.email || '',
             phone: auth.phone || '',
             company_name: auth.company_name || '',
-            date: new Date()
+            date: new Date(),
+            user_id: auth.id,
+            person: 1
         });     
         setSuccess(false)   
     }, [auth]);
 
 
+    function formatDateTime(date) {
+        const pad = (num) => String(num).padStart(2, '0');
+      
+        const year = date.getFullYear();
+        const month = pad(date.getMonth() + 1); // Aylar 0-indexli
+        const day = pad(date.getDate());
+        const hours = pad(date.getHours());
+        const minutes = pad(date.getMinutes());
+        const seconds = pad(date.getSeconds());
+      
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    };
+
+      
     async function HandleSubmit() {
-        const result = await MeetStoreApi({token: auth.token, data: form});
+        const result = await MeetStoreApi({
+            token: auth.token, 
+            data: {...form, date: formatDateTime(form.date)}
+        });
         if(result.status)
             setSuccess(true);
-
-        console.log(result)
+        if(!result.status)
+            setErrors(result.data);        
     }
     
     if(success) {
@@ -60,22 +79,30 @@ export default function MeetRequest() {
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Ad Soyad</Text>
                     <TextInput
-                        style={styleAuth.form.item.input}
+                        style={[
+                            styleAuth.form.item.input,
+                            errors?.name && { borderColor: 'red', color: 'red' }
+                        ]}
                         onChangeText={val => setForm({...form, name: val})}
                         value={form.name}
                         placeholder="Adınızı soyadınızı girin"
                         autoComplete="name"
                         placeholderTextColor="black"
                         inputMode="text"
-
                     />
+                    {errors?.name && (
+                        <Text style={{ color: 'red', marginTop: 5, fontSize: 12 }}>{errors?.name[0]}</Text>
+                    )}
                 </View>
 
                 {/* Eposta */}
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Eposta</Text>
                     <TextInput
-                        style={styleAuth.form.item.input}
+                        style={[
+                            styleAuth.form.item.input,
+                            errors?.email && { borderColor: 'red', color: 'red' }
+                        ]}
                         onChangeText={val => setForm({...form, email: val})}
                         value={form.email}
                         placeholder="Eposta adresinizi girin"
@@ -83,13 +110,19 @@ export default function MeetRequest() {
                         placeholderTextColor="black"
                         inputMode="email"
                     />
+                    {errors?.email && (
+                        <Text style={{ color: 'red', marginTop: 5, fontSize: 12 }}>{errors?.email[0]}</Text>
+                    )}
                 </View>
 
                 {/* Telefon */}
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Telefon</Text>
                     <TextInput
-                        style={styleAuth.form.item.input}
+                        style={[
+                            styleAuth.form.item.input,
+                            errors?.phone && { borderColor: 'red', color: 'red' }
+                        ]}
                         onChangeText={val => setForm({...form, phone: val})}
                         value={form.phone}
                         placeholder="Telefon numaranızı girin"
@@ -97,19 +130,28 @@ export default function MeetRequest() {
                         placeholderTextColor="black"
                         inputMode="tel"
                     />
+                    {errors?.phone && (
+                        <Text style={{ color: 'red', marginTop: 5, fontSize: 12 }}>{errors?.phone[0]}</Text>
+                    )}
                 </View>
 
                 {/* Şirket Adı */}
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Şirket Adı</Text>
                     <TextInput
-                        style={styleAuth.form.item.input}
+                        style={[
+                            styleAuth.form.item.input,
+                            errors?.company_name && { borderColor: 'red', color: 'red' }
+                        ]}
                         onChangeText={val => setForm({...form, company_name: val})}
                         value={form.company_name}
                         placeholder="Çalıştığınız Firma"
                         placeholderTextColor="black"
                         inputMode="text"
                     />
+                    {errors?.company_name && (
+                        <Text style={{ color: 'red', marginTop: 5, fontSize: 12 }}>{errors?.company_name[0]}</Text>
+                    )}
                 </View>
 
                 {/* Tarih */}
@@ -127,7 +169,7 @@ export default function MeetRequest() {
                             justifyContent: 'center',
                         }}
                     >
-                        <Text>{form.date.toLocaleDateString()}</Text>
+                        <Text>{form.date.toLocaleDateString('tr-TR')}</Text>
                     </TouchableOpacity>
 
                     {showDatePicker && (
@@ -138,26 +180,31 @@ export default function MeetRequest() {
                         onChange={(event, selectedDate) => {
                             setShowDatePicker(false);
                             if (selectedDate) {
-                            setForm({ ...form, date: selectedDate });
+                                setForm({ ...form, date: selectedDate.toISOString() });
                             }
                         }}
                         />
                     )}
                 </View>
 
-
                 {/* Kişi Sayısı */}
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Kişi Sayısı</Text>
                     <View style={{width: '100%', height: 50, borderWidth: 1, borderColor: '#C1C1C1', borderRadius: 10, padding: 0}}>
                         <TextInput
-                            style={styleAuth.form.item.input}
-                            onChangeText={val => setForm({...form, people: val})}
-                            value={form.people}
+                            style={[
+                                styleAuth.form.item.input,
+                                errors?.person && { borderColor: 'red' }
+                            ]}
+                            onChangeText={val => setForm({...form, person: val})}
+                            value={form.person}
                             placeholder="Kişi sayısı girin."
                             placeholderTextColor="black"
                             inputMode="numeric"
                         />
+                        {errors?.person && (
+                            <Text style={{ color: 'red', marginTop: 5 }}>{errors?.person[0]}</Text>
+                        )}
                     </View>                
                 </View>
 
--- app/views/MeetShowroomRequest.js
+++ app/views/MeetShowroomRequest.js
@@ -10,10 +10,11 @@ import { FontAwesome } from '@expo/vector-icons';
 import { MeetShowroomStoreApi } from "../api/MeetShowroomApi";
 
 export default function MeetShowroomRequest() {
-    const [form, setForm] = useState({name: '', email: '', phone: '', company_name: '', date: new Date()});
+    const [form, setForm] = useState({name: '', email: '', phone: '', company_name: '', date: new Date(), person: 1, user_id: 0, showroom: 'İstanbul Showroom"'});
     const auth = useSelector(state => state.auth.value);    
     const [showDatePicker, setShowDatePicker] = useState(false);
     const [success, setSuccess] = useState(false);
+    const [errors, setErrors] = useState({});
 
 
     useEffect(() => {
@@ -23,7 +24,10 @@ export default function MeetShowroomRequest() {
             email: auth.email || '',
             phone: auth.phone || '',
             company_name: auth.company_name || '',
-            date: new Date()
+            date: new Date(),
+            person: 1,
+            showroom: 'İstanbul Showroom"',
+            user_id: auth.id
         });    
         
     }, [auth]);
@@ -41,12 +45,29 @@ export default function MeetShowroomRequest() {
         )
     };
 
+    function formatDateTime(date) {
+        const pad = (num) => String(num).padStart(2, '0');
+      
+        const year = date.getFullYear();
+        const month = pad(date.getMonth() + 1); // Aylar 0-indexli
+        const day = pad(date.getDate());
+        const hours = pad(date.getHours());
+        const minutes = pad(date.getMinutes());
+        const seconds = pad(date.getSeconds());
+      
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    };
+
     async function HandleSubmit() {
-        const result = await MeetShowroomStoreApi({token: auth.token, data: form});
+        const result = await MeetShowroomStoreApi({
+            token: auth.token, 
+            data: {...form, date: formatDateTime(form.date)}
+        });
+        
         if(result.status)
             setSuccess(true);
-
-        console.log(result)
+        if(!result.status)
+            setErrors(result.data);        
     }
 
     return (
@@ -56,55 +77,79 @@ export default function MeetShowroomRequest() {
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Ad Soyad</Text>
                     <TextInput
-                        style={styleAuth.form.item.input}
-                        onChange={val => setForm({...form, name: val})}
+                        style={[
+                            styleAuth.form.item.input,
+                            errors?.name && { borderColor: 'red', color: 'red' }
+                        ]}
+                        onChangeText={val => setForm({...form, name: val})}
                         value={form.name}
                         placeholder="Adınızı soyadınızı girin"
                         autoComplete="name"
                         placeholderTextColor="black"
                         inputMode="text"
-
                     />
+                    {errors?.name && (
+                        <Text style={{ color: 'red', marginTop: 5, fontSize: 12 }}>{errors?.name[0]}</Text>
+                    )}
                 </View>
 
                 {/* Eposta */}
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Eposta</Text>
                     <TextInput
-                        style={styleAuth.form.item.input}
-                        onChange={val => setForm({...form, email: val})}
+                        style={[
+                            styleAuth.form.item.input,
+                            errors?.email && { borderColor: 'red', color: 'red' }
+                        ]}
+                        onChangeText={val => setForm({...form, email: val})}
                         value={form.email}
                         placeholder="Eposta adresinizi girin"
                         autoComplete="email"
                         placeholderTextColor="black"
                         inputMode="email"
                     />
+                    {errors?.email && (
+                        <Text style={{ color: 'red', marginTop: 5, fontSize: 12 }}>{errors?.email[0]}</Text>
+                    )}
                 </View>
 
                 {/* Telefon */}
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Telefon</Text>
                     <TextInput
-                        style={styleAuth.form.item.input}
-                        onChange={val => setForm({...form, phone: val})}
+                        style={[
+                            styleAuth.form.item.input,
+                            errors?.phone && { borderColor: 'red', color: 'red' }
+                        ]}
+                        onChangeText={val => setForm({...form, phone: val})}
                         value={form.phone}
                         placeholder="Telefon numaranızı girin"
                         autoComplete="tel"
                         placeholderTextColor="black"
                         inputMode="tel"
                     />
+                    {errors?.phone && (
+                        <Text style={{ color: 'red', marginTop: 5, fontSize: 12 }}>{errors?.phone[0]}</Text>
+                    )}
                 </View>
 
                 {/* Şirket Adı */}
                 <View style={{marginBottom: 20}}>
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Şirket Adı</Text>
                     <TextInput
-                        style={styleAuth.form.item.input}
-                        onChange={val => setForm({...form, company_name: val})}
+                        style={[
+                            styleAuth.form.item.input,
+                            errors?.company_name && { borderColor: 'red', color: 'red' }
+                        ]}
+                        onChangeText={val => setForm({...form, company_name: val})}
                         value={form.company_name}
                         placeholder="Çalıştığınız Firma"
                         placeholderTextColor="black"
+                        inputMode="text"
                     />
+                    {errors?.company_name && (
+                        <Text style={{ color: 'red', marginTop: 5, fontSize: 12 }}>{errors?.company_name[0]}</Text>
+                    )}
                 </View>
 
                 {/* Showroom */}
@@ -137,7 +182,7 @@ export default function MeetShowroomRequest() {
                             justifyContent: 'center',
                         }}
                     >
-                        <Text>{form.date.toLocaleDateString()}</Text>
+                        <Text>{form.date.toLocaleDateString('tr-TR')}</Text>
                     </TouchableOpacity>
 
                     {showDatePicker && (
@@ -148,7 +193,7 @@ export default function MeetShowroomRequest() {
                         onChange={(event, selectedDate) => {
                             setShowDatePicker(false);
                             if (selectedDate) {
-                            setForm({ ...form, date: selectedDate });
+                                setForm({ ...form, date: selectedDate.toISOString() });
                             }
                         }}
                         />
@@ -160,13 +205,19 @@ export default function MeetShowroomRequest() {
                     <Text style={{fontWeight: 700, fontSize: 16, marginBottom: 3}}>Kişi Sayısı</Text>
                     <View style={{width: '100%', height: 50, borderWidth: 1, borderColor: '#C1C1C1', borderRadius: 10, padding: 0}}>
                         <TextInput
-                            style={styleAuth.form.item.input}
-                            onChangeText={val => setForm({...form, people: val})}
-                            value={form.people}
+                            style={[
+                                styleAuth.form.item.input,
+                                errors?.person && { borderColor: 'red' }
+                            ]}
+                            onChangeText={val => setForm({...form, person: val})}
+                            value={form.person}
                             placeholder="Kişi sayısı girin."
                             placeholderTextColor="black"
                             inputMode="numeric"
                         />
+                        {errors?.person && (
+                            <Text style={{ color: 'red', marginTop: 5 }}>{errors?.person[0]}</Text>
+                        )}
                     </View>                
                 </View>
 
